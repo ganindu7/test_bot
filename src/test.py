0ccd1dbf517f55a9168b0aabbe466dd9438f3040
Ganindu Nanayakkara <gnanayakkara@rdmgroup.co.uk>
2018-01-20 17:43:21 +0000

    checking stuff

--- src/test.py
+++ src/test.py
@@ -2,4 +2,8 @@ import ccxt
 
 print(ccxt.exchanges)
 
-exchange = ccxt.binance()
+exchange = ccxt.gdax()
+
+print(exchange.id, exchange.load_markets())
+print(exchange.fetch_order_book(exchange.symbols[0]))
+print(exchange.fetch_ticker('LTC/EUR'))
