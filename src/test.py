c80c36a3ababa2534adf447023a5c79c221fbabd
Ganindu Nanayakkara <gnanayakkara@rdmgroup.co.uk>
2018-01-20 20:46:40 +0000

    housekeeping

--- src/test.py
+++ src/test.py
@@ -4,15 +4,6 @@ import sys
 import asciichart
 
 
-# print(ccxt.exchanges)
-# 
-# exchange = ccxt.gdax()
-# 
-# print(exchange.id, exchange.load_markets())
-# print(exchange.fetch_order_book(exchange.symbols[0]))
-# print(exchange.fetch_ticker('LTC/EUR'))
-
-
 this_folder = os.path.dirname(os.path.abspath(__file__))
 root_folder = os.path.dirname(this_folder)
 sys.path.append(root_folder + '/python')
