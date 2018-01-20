26ceb8319bdb42183d4c7bc648317e0e9a603857
Ganindu Nanayakkara <gnanayakkara@rdmgroup.co.uk>
2018-01-20 18:43:44 +0000

    testing stuff

--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
 import ccxt
+import os
+import sys
 
 print(ccxt.exchanges)
 
@@ -7,4 +9,11 @@ exchange = ccxt.gdax()
 print(exchange.id, exchange.load_markets())
 print(exchange.fetch_order_book(exchange.symbols[0]))
 print(exchange.fetch_ticker('LTC/EUR'))
-print('key test');
+
+
+this_folder = os.path.dirname(os.path.abspath(__file__))
+print(this_folder)
+root_folder = os.path.dirname(this_folder)
+print(root_folder)
+print(sys.path)
+
