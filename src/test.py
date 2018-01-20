67489e9001f2287a1d1de21619edc42881c24862
Ganindu Nanayakkara <gnanayakkara@rdmgroup.co.uk>
2018-01-20 20:32:45 +0000

    added asciichart from https://github.com/ccxt/ccxt/blob/master/examples/py/asciichart.py
    and a demo chart

--- src/test.py
+++ src/test.py
@@ -1,19 +1,44 @@
 import ccxt
 import os
 import sys
+import asciichart
 
-print(ccxt.exchanges)
 
-exchange = ccxt.gdax()
-
-print(exchange.id, exchange.load_markets())
-print(exchange.fetch_order_book(exchange.symbols[0]))
-print(exchange.fetch_ticker('LTC/EUR'))
+# print(ccxt.exchanges)
+# 
+# exchange = ccxt.gdax()
+# 
+# print(exchange.id, exchange.load_markets())
+# print(exchange.fetch_order_book(exchange.symbols[0]))
+# print(exchange.fetch_ticker('LTC/EUR'))
 
 
 this_folder = os.path.dirname(os.path.abspath(__file__))
-print(this_folder)
 root_folder = os.path.dirname(this_folder)
-print(root_folder)
-print(sys.path)
+sys.path.append(root_folder + '/python')
+sys.path.append(this_folder)   # append to path 
+
+ 
+gdax = ccxt.gdax()
+symbol = 'LTC/EUR'
+
+# an ohlcv candle has six items-> open, close, high, low prices then volume and timestamp 
+# in order [timestamp, open, high, low, close, volume]
+
+def print_chart(exchange, symbol, timeframe):
+    
+    print("\n" + exchange.name + ' ' + timeframe + 'chart: ' )
+    
+    # get a list of ohlcv candles
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
+    
+    # get the attribute we are interested in
+    index = 4 
+    series = [x[index] for x in ohlcv]
+    
+    # print chart
+    print("\n" + asciichart.plot(series[-120:], {'height': 20}))
+    
+print_chart(gdax,symbol, '1h')
+
 
