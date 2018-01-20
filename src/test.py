66888ed66b187c3c44bf4a8e64b39e5b645fb2f8
Ganindu Nanayakkara <gnanayakkara@rdmgroup.co.uk>
2018-01-20 17:47:48 +0000

    key add test

--- src/test.py
+++ src/test.py
@@ -7,3 +7,4 @@ exchange = ccxt.gdax()
 print(exchange.id, exchange.load_markets())
 print(exchange.fetch_order_book(exchange.symbols[0]))
 print(exchange.fetch_ticker('LTC/EUR'))
+print('key test');
